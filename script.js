82e52d9c92cc9b00022a645925a02f3731c4f9c5
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-17 23:31:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let n1 = s1.length;
+	let n2 = s2.length;
+	
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    let ans = -1;
+
+    for(let i =0 ; i<=n1-n2 ; i++){
+		let str = s1.slice(i,i+n2);
+		if(str === s2){
+			ans = i;
+			break;
+		}
+	}
+	return ans;
 }
 
 // Please do not change the code below
